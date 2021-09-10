9da3074cd0bcc4596059bbc79e339cb40d326490
hyowon <khw0107799@naver.com>
2021-09-11 01:03:27 +0900

    small screen

--- WebContent/front/html/main.js
+++ WebContent/front/html/main.js
@@ -1,7 +1,11 @@
 //메뉴바 클릭
 const togglebtn = document.querySelector('.navbar_togglebtn');
 const menu = document.querySelector('.navbar_menu');
+const visual_inner = document.querySelector('.visual_inner');
 
 togglebtn.addEventListener('click', () => {
   menu.classList.toggle('active');
+  visual_inner.classList.toggle('active');
 });
+
+
